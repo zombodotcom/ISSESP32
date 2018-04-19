24f8baeaa3771d88b1c5ba8307ef53a5803f8dbc
zombodotcom <tommysciano@gmail.com>
2018-04-19 11:37:44 -0600

    FASTLED Edit
    FASTLED Edit

---
+++ ZomboISS-ESP32FastLED/fastledsetup.h
@@ -0,0 +1,91 @@
+#include <stdlib.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+#include "driver/gpio.h"
+#include "driver/adc.h"
+#include "esp_system.h"
+#include "esp_spi_flash.h"
+
+
+
+
+#include "FastLED.h"
+FASTLED_USING_NAMESPACE
+
+//led stuff
+#define DATA_PINLL   25
+//#define DATA_PINRR   27
+//#define DATA_PINRM   33
+
+//#define CLK_PIN   4
+#define LED_TYPE    WS2812B
+#define COLOR_ORDER GRB
+#define NUM_LEDS   32
+CRGB leds[NUM_LEDS];
+#define BRIGHTNESS         20
+#define FRAMES_PER_SECOND  120
+CRGB solidColor = CRGB::Blue;
+#define FASTLED_SHOW_CORE 0
+
+
+// -- Task handles for use in the notifications
+static TaskHandle_t FastLEDshowTaskHandle = 0;
+static TaskHandle_t userTaskHandle = 0;
+
+/** show() for ESP32
+    Call this function instead of FastLED.show(). It signals core 0 to issue a show,
+    then waits for a notification that it is done.
+*/
+void FastLEDshowESP32()
+{
+  if (userTaskHandle == 0) {
+    // -- Store the handle of the current task, so that the show task can
+    //    notify it when it's done
+    userTaskHandle = xTaskGetCurrentTaskHandle();
+
+    // -- Trigger the show task
+    xTaskNotifyGive(FastLEDshowTaskHandle);
+
+    // -- Wait to be notified that it's done
+    const TickType_t xMaxBlockTime = pdMS_TO_TICKS( 200 );
+    ulTaskNotifyTake(pdTRUE, xMaxBlockTime);
+    userTaskHandle = 0;
+  }
+}
+
+/** show Task
+    This function runs on core 0 and just waits for requests to call FastLED.show()
+*/
+void FastLEDshowTask(void *pvParameters)
+{
+  // -- Run forever...
+  for (;;) {
+    // -- Wait for the trigger
+    ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
+
+    // -- Do the show (synchronously)
+    FastLED.show();
+
+    // -- Notify the calling task
+    xTaskNotifyGive(userTaskHandle);
+  }
+}
+
+
+void print_chip_info()
+{
+  esp_chip_info_t chip_info;
+  esp_chip_info(&chip_info);
+  printf("This is ESP32 chip with %d CPU cores, WiFi%s%s, ",
+         chip_info.cores,
+         (chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "",
+         (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "");
+
+  printf("silicon revision %d, ", chip_info.revision);
+
+  printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
+         (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
+}
+
+
